f9c2821d7d97baa3711d540ff725d5ba4f654735
jaroslav-zukov <jaroslav.zukov@gmail.com>
2024-05-08 18:02:07 +0200

    returning to plotly and dash-bio

---
+++ plotly-test.py
@@ -0,0 +1,67 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+import pysam
+import dash_bio as dashbio
+import urllib.request as urlreq
+import json
+
+bamfile = pysam.AlignmentFile("calls.bam", "rb", check_sq=False)
+read_count = bamfile.count(until_eof=True)
+bamfile.close()
+
+def get_read_id(read_number):
+    bamfile = pysam.AlignmentFile("calls.bam", "rb", check_sq=False)
+    counter = 0
+    for read in bamfile:
+        counter += 1
+        if counter == read_number:
+            return read.query_name
+        
+data = ">6a4800da-842d-4702-9a8e-8bf3bcc264b9\nGAAAGAAGCTGAGGAGAAGAGCGATTCTGTAGGAAGACCAGCAGTCTCAATTAATATGGACCTCTGAGATCTTTCAAATACTGGACAACCAAACAGACAGCATATACCAGCTGATATGAGGCCCCAAACATACATACAGAAAAGGAATTCCAAGTCTTTGTTCATTCAGGTGCTGTCGATTCCGTTTGTAGTCGTCTGTTTAACCTTAGCAATACGTG"       
+
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H1(children='Trying visualising stuff', style={'textAlign':'center'}),
+    dcc.Slider(1, read_count, 1, value=1, id='slider-selection', marks=None),
+    html.Div(id='slider-output-container'),
+    dashbio.AlignmentChart(
+        id='alignment-viewer',
+        data=data,
+        height=200,
+        tilewidth=10,
+        tileheight=15,
+        textsize=10,
+        showconservation=False,
+        showconsensus=False,
+        showgap=False,
+    ),
+    # html.Div(id='alignment-viewer-output')
+])
+
+@callback(
+    Output('slider-output-container', 'children'),
+    Input('slider-selection', 'value'))
+def update_output(value):
+    return 'You have selected read number {} with read_id {}'.format(value, get_read_id(value))
+
+
+@callback(
+    Output('alignment-viewer', 'data'),
+    Input('slider-selection', 'value'))
+def update_alignment_chart(value):
+    result = ""
+    read_id = get_read_id(value)
+    fastafile = pysam.FastaFile("calls.fasta")
+    read = fastafile.fetch(read_id)
+    result += ">{}\n".format(read_id)
+    result += read
+    
+    return result
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- pysam-test.py
+++ pysam-test.py
@@ -3,17 +3,31 @@ import pysam
 # Open the unaligned BAM file
 bamfile = pysam.AlignmentFile("calls.bam", "rb", check_sq=False)
 
-read = next(bamfile)
-print(read.query_name)
-print(read.query_sequence)
-print(read.query_qualities)
-print(read.get_tag("MM"))
-print(read.get_tag("ML"))
+# read = next(bamfile)
+# print(read.query_name)
+# print(read.query_sequence)
+# print(read.query_qualities)
+# print(read.get_tag("MM"))
+# print(read.get_tag("ML"))
 
-query_qualities = read.query_qualities
+# query_qualities = read.query_qualities
 
-for i in range(len(query_qualities)):
-    print(query_qualities[i])
+# for i in range(len(query_qualities)):
+#     print(query_qualities[i])
+
+print(bamfile.count(until_eof=True))
+
+fastafile = pysam.FastaFile("calls.fasta")
+
+read = fastafile.fetch("6a4800da-842d-4702-9a8e-8bf3bcc264b9")
+
+# Now 'read' contains the sequence of the read with the specified ID
+print(read)
+
+
+
+
+fastafile.close()
 
 bamfile.close()
 
