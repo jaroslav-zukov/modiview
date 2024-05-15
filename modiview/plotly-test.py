80ba7f5991f8199b1538a968f7d89908fd7899b6
jaroslav-zukov <jaroslav.zukov@gmail.com>
2024-05-15 15:56:40 +0200

    added modifications dropdown

--- modiview/plotly-test.py
+++ modiview/plotly-test.py
@@ -5,7 +5,7 @@ import dash
 import dash_bio as dashbio
 import numpy as np
 import pandas as pd
-import plotly.express as px
+import plotly.graph_objects as go
 from dash import Dash, html, dcc, callback, Output, Input, no_update
 
 import modifications
@@ -15,6 +15,15 @@ bam_handler = BAMFileHandler("/Users/jaroslav/Projects/modiview/calls.bam")
 
 read_count = bam_handler.get_read_count()
 initial_fasta_sequence = bam_handler.get_fasta_sequence(1)
+
+modifications_dict = {
+    "C+m?": "5-Methylcytosine",
+    "C+h?": "5-Hydroxymethylcytosine",
+}
+reverse_modifications_dict = {v: k for k, v in modifications_dict.items()}
+
+initial_modifications_short = list(modifications.get_modifications(bam_handler.get_read(1)).keys())
+initial_modifications_full = [modifications_dict[mod] for mod in initial_modifications_short]
 
 app = Dash(__name__)
 
@@ -30,6 +39,13 @@ app.layout = html.Div([
         style={'textAlign': 'center', 'width': '5%'},
         value=1),
     html.Div(id='read-num-output-container'),
+    "Select modifications:",
+    dcc.Dropdown(
+        options=initial_modifications_full,
+        value=initial_modifications_full,
+        multi=True,
+        id='mod_dropdown'
+    ),
     dashbio.AlignmentChart(
         id='alignment-viewer',
         data=initial_fasta_sequence,
@@ -66,8 +82,10 @@ def update_alignment_chart(value):
 @callback(
     Output('modifications-plot', 'figure'),
     Input('alignment-viewer', 'eventDatum'),
-    Input('range', 'value'))
-def update_modifications_plot(alignment_viewer_event, read_number):
+    Input('range', 'value'),
+    Input('mod_dropdown', 'value'),
+)
+def update_modifications_plot(alignment_viewer_event, read_number, mods_selected):
     read = bam_handler.get_read(read_number)
     sequence = read.query_sequence
     trigger_id = dash.callback_context.triggered[0]['prop_id'].split('.')[0]
@@ -80,29 +98,55 @@ def update_modifications_plot(alignment_viewer_event, read_number):
         if 'eventType' in parsed_event and parsed_event['eventType'] == 'Zoom':
             start = math.ceil(parsed_event['xStart'])
             end = math.floor(parsed_event['xEnd'])
-            # Just not to throw value out of range error
             if start < 0 or end > nucleotides_shown:
                 return no_update
         else:
             return no_update
 
-    methylation_positions = modifications.get_modifications(read)['C+m?']
+    fig = go.Figure()
+    mods = modifications.get_modifications(read)
+    x_values = list(range(start + 1, end + 1))
+
+    for modification in mods_selected:
+        y_values = generate_modification_list(
+            mods[reverse_modifications_dict[modification]],
+            nucleotides_shown
+        )[start:end]
+        df = pd.DataFrame({
+            'x': x_values,
+            'y': y_values,
+        })
+        fig.add_trace(
+            go.Scatter(
+                x=df['x'],
+                y=df['y'],
+                mode='lines',
+                name=modification
+            )
+        )
+
+    fig.update_layout(
+        autosize=False,
+        width=2605,
+        height=300,
+        yaxis=dict(range=[0, 1]),
+        legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        )
+    )
+
+    return fig
+
+
+def generate_modification_list(methylation_positions, nucleotides_shown):
     result = np.zeros(nucleotides_shown)
     for position, probability in methylation_positions:
         result[position] += probability
-
-    x_values = list(range(start + 1, end + 1))
-    y_values = result.tolist()[start:end]
-
-    df = pd.DataFrame({
-        'x': x_values,
-        'y': y_values,
-    })
-
-    fig = px.line(df, x='x', y='y', height=300)
-    fig.update_layout(autosize=False, width=2605, coloraxis_showscale=False, yaxis=dict(range=[0, 1]))
-
-    return fig
+    return result.tolist()
 
 
 if __name__ == '__main__':
